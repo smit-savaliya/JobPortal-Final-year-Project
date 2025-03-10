372d1329fdb00f930397f4a256975dbb39c46e1e
smit-savaliya <smit.savaliya1717@gmail.com>
2025-03-10 11:09:59 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express, { json } from "express"
+import cors from "cors"
+import "dotenv/config"
+import { db } from "./config/db.js"
+import { clerkwebhooks } from "./controllers/webhooks.js"
+
+const app = express()
+
+//Middlware
+app.use(cors())
+app.use(express.json())
+
+//Route
+app.get("/", (req,res)=>{
+    res.send("Api working")
+})
+app.post("/webhooks", clerkwebhooks)
+
+//Port
+const PORT =  process.env.PORT || 5050
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
